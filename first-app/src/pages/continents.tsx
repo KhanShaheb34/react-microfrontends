fd6ac753c65e5dfb429501daaebecec1039b9662
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-06-02 15:49:00 +0600

    Continent design done

--- first-app/src/pages/continents.tsx
+++ first-app/src/pages/continents.tsx
@@ -1,3 +1,32 @@
+import { Box, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export type ContinentType = {
+  id: number;
+  name: string;
+};
+
+const fetchCharacters = async () => {
+  const url = "https://thronesapi.com/api/v2/Continents";
+  const response = await axios.get<ContinentType[]>(url);
+  return response.data;
+};
+
 export const ContinentsPage = () => {
-  return <h1>ContinentsPage</h1>;
+  const [continents, setContinents] = useState<ContinentType[]>([]);
+
+  useEffect(() => {
+    fetchCharacters().then(setContinents);
+  }, []);
+
+  return (
+    <Box display="flex" justifyContent="space-around" flexWrap="wrap">
+      {continents.map((item) => (
+        <Box border="1px solid #ddd" padding={20} margin={10}>
+          <Text>{item.name}</Text>
+        </Box>
+      ))}
+    </Box>
+  );
 };
