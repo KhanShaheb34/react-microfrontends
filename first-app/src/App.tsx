06f0c4e55e102bde87facf435a2fd25dcc6b9ad6
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-06-02 13:54:40 +0600

    First app route setup

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/home";
+import { CharactersPage } from "./pages/characters";
+import { ContinentsPage } from "./pages/continents";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Welcome to GOT App</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="character" element={<CharactersPage />} />
+        <Route path="continent" element={<ContinentsPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ first-app/src/pages/continents.tsx
@@ -0,0 +1,3 @@
+export const ContinentsPage = () => {
+  return <h1>ContinentsPage</h1>;
+};
