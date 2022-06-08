3297d65e612045aacd0720a848e7c80aff1e38e1
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-06-08 15:40:30 +0600

    Script to build all app and run server

---
+++ local-build.sh
@@ -0,0 +1,24 @@
+echo "============== CLEANING =============="
+rm -rf ./build
+rm -rf ./*/dist
+
+echo "============== BUILDING =============="
+(cd first-app && npm run build)
+(cd second-app && npm run build)
+(cd navbar && npm run build)
+(cd root-config && npm run build)
+
+echo "============== COPYING ==============="
+mkdir -p ./build/got ./build/rnm ./build/nav
+cp -r ./first-app/dist/. ./build/got
+cp -r ./second-app/dist/. ./build/rnm
+cp -r ./navbar/dist/. ./build/nav
+cp -r ./root-config/dist/. ./build
+
+echo "============== CLEANING =============="
+rm -rf ./*/dist
+
+echo "============ START SERVER ============"
+npx live-server ./build --port=9001 --host=localhost
+
+echo "================ DONE ================"
